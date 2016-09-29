1c450c72cddd38ab9890af51c87fa241cd7c20bb
James Navin <jnavin@atlassian.com>
2016-09-29 17:17:00 +1000

    #20: Added Javadoc

--- swagger-request-validator-pact/src/main/java/com/atlassian/oai/validator/pact/PactLevelResolverFactory.java
+++ swagger-request-validator-pact/src/main/java/com/atlassian/oai/validator/pact/PactLevelResolverFactory.java
@@ -3,8 +3,18 @@ package com.atlassian.oai.validator.pact;
 import com.atlassian.oai.validator.report.LevelResolver;
 import com.atlassian.oai.validator.report.ValidationReport;
 
+/**
+ * A factory for creating {@link LevelResolver} instances that are suitable for use in Pact validation scenarios.
+ *
+ * @see #create()
+ */
 public class PactLevelResolverFactory {
 
+    private PactLevelResolverFactory(){}
+
+    /**
+     * Create a {@link LevelResolver} instance that is suitable for use in Pact validation scenarios.
+     */
     public static LevelResolver create() {
         return LevelResolver.create()
                 .withLevel("validation.schema.required", ValidationReport.Level.INFO)
--- swagger-request-validator-pact/src/main/java/com/atlassian/oai/validator/pact/PactProviderValidator.java
+++ swagger-request-validator-pact/src/main/java/com/atlassian/oai/validator/pact/PactProviderValidator.java
@@ -26,6 +26,34 @@ import static java.util.Collections.emptyMap;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toMap;
 
+/**
+ * A validator that can be used on the Provider side to validate Consumer Pacts against the
+ * Provider's Swagger API specification.
+ * <p>
+ * The validator can be used to validate against all Consumers with Pacts registered in a broker, and/or
+ * against individual Consumer Pact files sourced from other locations (the file system etc.)
+ * <p>
+ * To validate against all Consumers in a broker:
+ * <pre>
+ *     final PactProviderValidator validator = PactProviderValidator
+ *                                                  .createFor(SWAGGER_JSON_URL)
+ *                                                  .withPactsFrom(BROKER_URL, PROVIDER_ID)
+ *                                                  .build();
+ * </pre>
+ *
+ * To validate against specific Consumer Pact files:
+ * <pre>
+ *     final PactProviderValidator validator = PactProviderValidator
+ *                                                  .createFor(SWAGGER_JSON_URL)
+ *                                                  .withConsumer(CONSUMER_ID, CONSUMER_PACT_URL)
+ *                                                  .build();
+ * </pre>
+ *
+ *
+ * @see <a href="https://docs.pact.io/documentation/sharings_pacts.html">Pact broker</a>
+ * @see SwaggerRequestResponseValidator
+ * @see ValidatedPactProviderRule
+ */
 public class PactProviderValidator {
 
     private static final Logger log = LoggerFactory.getLogger(PactProviderValidator.class);
@@ -47,11 +75,24 @@ public class PactProviderValidator {
         }
     }
 
+    /**
+     * Create a new {@link PactProviderValidator} that validates Consumers against the given Swagger API specification.
+     *
+     * @param swaggerJsonUrl The URL of the Swagger API specification to use
+     *
+     * @return A builder that can create configured {@link PactProviderValidator} instances.
+     */
     public static Builder createFor(@Nonnull final String swaggerJsonUrl) {
         return new Builder().withSwaggerJsonUrl(swaggerJsonUrl);
     }
 
+    /**
+     * Perform the validation of Consumer Pacts against the configured Swagger API specification.
+     *
+     * @return A map that contains the {@link ValidationReport} for each Consumer.
+     */
     public Map<ConsumerInfo, ValidationReport> validate() {
+        log.debug("Validating {} consumers against API spec", consumers.size());
         if (consumers.isEmpty()) {
             log.warn("No consumers supplied. No validation will be performed.");
             return emptyMap();
@@ -81,6 +122,13 @@ public class PactProviderValidator {
         return new AbstractMap.SimpleEntry<>(consumer, report);
     }
 
+    /**
+     * A builder that can be used to create configured {@link PactProviderValidator} instances.
+     * <p>
+     * Instances should normally be obtained via {@link PactProviderValidator#createFor(String)}.
+     *
+     * @see PactProviderValidator#createFor(String)
+     */
     public static class Builder {
 
         private String swaggerJsonUrl;
@@ -89,32 +137,91 @@ public class PactProviderValidator {
         private String brokerUrl;
         private String providerName;
 
+        /**
+         * The location of the Swagger JSON specification to use in the validator.
+         * <p>
+         * The URL can be an absolute HTTP/HTTPS URL, a File URL or a classpath location (without the classpath: scheme).
+         * <p>
+         * For example:
+         * <pre>
+         *     // Create from a publicly hosted HTTP location
+         *     .withSwaggerJsonUrl("http://api.myservice.com/swagger.json")
+         *
+         *     // Create from a file on the local filesystem
+         *     .withSwaggerJsonUrl("file://Users/myuser/tmp/swagger.json");
+         *
+         *     // Create from a classpath resource in the /api package
+         *     .withSwaggerJsonUrl("/api/swagger.json");
+         * </pre>
+         * @param swaggerJsonUrl The location of the Swagger JSON specification to use in the validator.
+         *
+         * @return this builder instance.
+         */
         public Builder withSwaggerJsonUrl(final String swaggerJsonUrl) {
             this.swaggerJsonUrl = swaggerJsonUrl;
             return this;
         }
 
+        /**
+         * Add one or more Consumers that will be included in the validation.
+         * <p>
+         * Note that each supplied consumer must have a <code>name</code> and <code>pactFile</code> configured.
+         *
+         * @param consumers The consumers to include
+         *
+         * @return this builder instance.
+         */
         public Builder withConsumers(final ConsumerInfo... consumers) {
             this.consumers.addAll(asList(consumers));
             return this;
         }
 
+        /**
+         * Add a Consumer that will be included in the validation.
+         *
+         * @param consumerName The name of the Consumer
+         * @param pactFileUrl The location of the Consumer Pact file to validate against
+         *
+         * @return this builder instance.
+         */
         public Builder withConsumer(final String consumerName, final String pactFileUrl) {
             this.consumers.add(new ConsumerInfo(consumerName, pactFileUrl));
             return this;
         }
 
+        /**
+         * Add a Consumer that will be included in the validation.
+         *
+         * @param consumerName The name of the Consumer
+         * @param pactFileUrl The location of the Consumer Pact file to validate against
+         *
+         * @return this builder instance.
+         */
         public Builder withConsumer(final String consumerName, final URL pactFileUrl) {
             this.consumers.add(new ConsumerInfo(consumerName, pactFileUrl));
             return this;
         }
 
+        /**
+         * Configure the validator to validate against all Consumer Pacts retrieved from the given
+         * broker for the given Provider.
+         *
+         * @param brokerUrl the URL of the Pact Broker to retrieve Consumer Pacts from
+         * @param providerName The ID of the Provider to retrieve Pacts for
+         *
+         * @return this builder instance.
+         */
         public Builder withPactsFrom(final String brokerUrl, final String providerName) {
             this.brokerUrl = brokerUrl;
             this.providerName = providerName;
             return this;
         }
 
+        /**
+         * Build a configured {@link PactProviderValidator} instance with the values collected in this builder.
+         *
+         * @return The configured {@link PactProviderValidator} instance.
+         */
         public PactProviderValidator build() {
             if (brokerUrl != null && providerName != null) {
                 consumers.addAll(retrieveConsumers(brokerUrl, providerName));
