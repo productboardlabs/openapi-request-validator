767ce33f3c15af6b2ff9a494a9872dd667a9a2b0
James Navin <jnavin@atlassian.com>
2017-02-28 15:53:42 +1100

    #22: Refactored the response validator to make it cleaner to add more validations

--- swagger-request-validator-core/src/main/java/com/atlassian/oai/validator/interaction/ResponseValidator.java
+++ swagger-request-validator-core/src/main/java/com/atlassian/oai/validator/interaction/ResponseValidator.java
@@ -3,7 +3,6 @@ package com.atlassian.oai.validator.interaction;
 import com.atlassian.oai.validator.model.ApiOperation;
 import com.atlassian.oai.validator.model.Response;
 import com.atlassian.oai.validator.report.MessageResolver;
-import com.atlassian.oai.validator.report.MutableValidationReport;
 import com.atlassian.oai.validator.report.ValidationReport;
 import com.atlassian.oai.validator.schema.SchemaValidator;
 
@@ -48,25 +47,31 @@ public class ResponseValidator {
             apiResponse = apiOperation.getOperation().getResponses().get("default"); // try the default response
         }
 
-        final MutableValidationReport validationReport = new MutableValidationReport();
         if (apiResponse == null) {
-            return validationReport.add(
+            return ValidationReport.singleton(
                     messages.get("validation.response.status.unknown",
                             response.getStatus(), apiOperation.getPathString().original())
             );
         }
 
+        return validateResponseBody(response, apiResponse, apiOperation);
+    }
+
+    @Nonnull
+    private ValidationReport validateResponseBody(@Nonnull final Response response,
+                                                  @Nonnull final io.swagger.models.Response apiResponse,
+                                                  @Nonnull final ApiOperation apiOperation) {
         if (apiResponse.getSchema() == null) {
-            return validationReport;
+            return ValidationReport.EMPTY_REPORT;
         }
 
         if (!response.getBody().isPresent() || response.getBody().get().isEmpty()) {
-            return validationReport.add(
+            return ValidationReport.singleton(
                     messages.get("validation.response.body.missing",
                             apiOperation.getMethod(), apiOperation.getPathString().original())
             );
         }
 
-        return validationReport.merge(schemaValidator.validate(response.getBody().get(), apiResponse.getSchema()));
+        return schemaValidator.validate(response.getBody().get(), apiResponse.getSchema());
     }
 }
