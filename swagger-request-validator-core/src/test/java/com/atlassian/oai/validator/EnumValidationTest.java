ca0c80d56dde5d41fa5e2ef6b1b236d3b78827fe
Sven Döring <sven.doering.new@gmail.com>
2018-06-14 16:14:27 +0200

    Issue 118: add missing enum test for enums of strings

--- swagger-request-validator-core/src/test/java/com/atlassian/oai/validator/EnumValidationTest.java
+++ swagger-request-validator-core/src/test/java/com/atlassian/oai/validator/EnumValidationTest.java
@@ -18,6 +18,60 @@ public class EnumValidationTest {
             SwaggerRequestResponseValidator.createFor("/oai/api-with-enum.yaml").build();
 
     @Test
+    public void test_validEnumStringValues() {
+        // setup:
+        final Request request = new SimpleRequest.Builder(Request.Method.POST, "/enumString/1")
+                .withHeader("headerEnum", "10")
+                .withQueryParam("queryEnum", "100")
+                .withBody("{\n" +
+                        "\"innerEnum\": \"1000\",\n" +
+                        "\"arrayWithInnerEnum\": [\"10000\"],\n" +
+                        "\"arrayWithOuterEnum\": [\"100000\"]\n" +
+                        "}")
+                .build();
+
+        // when:
+        final ValidationReport result = classUnderTest.validateRequest(request);
+
+        // then:
+        assertThat(result.hasErrors(), is(false));
+    }
+
+    @Test
+    public void test_invalidEnumStringValues() {
+        // setup:
+        final Request request = new SimpleRequest.Builder(Request.Method.POST, "/enumString/4")
+                .withHeader("headerEnum", "40")
+                .withQueryParam("queryEnum", "400")
+                .withBody("{\n" +
+                        "\"innerEnum\": \"4000\",\n" +
+                        "\"arrayWithInnerEnum\": [\"40000\"],\n" +
+                        "\"arrayWithOuterEnum\": [\"400000\"]\n" +
+                        "}")
+                .build();
+
+        // when:
+        final ValidationReport result = classUnderTest.validateRequest(request);
+
+        // then:
+        assertThat(result.hasErrors(), is(true));
+        assertThat(result.getMessages(), hasSize(6));
+        assertThat(
+                result.getMessages().stream()
+                        .map(ValidationReport.Message::getMessage).sorted()
+                        .collect(Collectors.toList()),
+                contains(
+                        "Value '4' for parameter 'pathEnum' is not allowed. Allowed values are <[1, 2]>.",
+                        "Value '40' for parameter 'headerEnum' is not allowed. Allowed values are <[10, 20]>.",
+                        "Value '400' for parameter 'queryEnum' is not allowed. Allowed values are <[100, 200]>.",
+                        "[Path '/arrayWithInnerEnum/0'] Instance value (\"40000\") not found in enum (possible values: [\"10000\",\"20000\"])",
+                        "[Path '/arrayWithOuterEnum/0'] Instance value (\"400000\") not found in enum (possible values: [\"100000\",\"200000\"])",
+                        "[Path '/innerEnum'] Instance value (\"4000\") not found in enum (possible values: [\"1000\",\"2000\"])"
+                )
+        );
+    }
+
+    @Test
     public void test_validEnumIntegerValues() {
         // setup:
         final Request request = new SimpleRequest.Builder(Request.Method.POST, "/enumInteger/1")
