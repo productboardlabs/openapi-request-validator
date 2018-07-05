e0f0b36c160a5e3b9ca820b2714e0ea902968b42
James Navin <jnavin@atlassian.com>
2018-07-05 18:47:26 +1000

    #131: Changed to test for explicit validation errors rather than just an exception

--- swagger-request-validator-pact/src/test/java/com/atlassian/oai/validator/pact/PactConsumerValidationTest.java
+++ swagger-request-validator-pact/src/test/java/com/atlassian/oai/validator/pact/PactConsumerValidationTest.java
@@ -1,11 +1,14 @@
 package com.atlassian.oai.validator.pact;
 
+//CHECKSTYLE:OFF IllegalImport
 import au.com.dius.pact.consumer.Pact;
 import au.com.dius.pact.consumer.PactVerification;
 import au.com.dius.pact.consumer.dsl.DslPart;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.model.PactFragment;
+import com.atlassian.oai.validator.pact.ValidatedPactProviderRule.PactValidationError;
+import com.atlassian.oai.validator.report.ValidationReport.Message;
 import junit.framework.AssertionFailedError;
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,9 +20,13 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Set;
 
 import static io.restassured.RestAssured.get;
 import static java.lang.String.format;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toSet;
+//CHECKSTYLE:ON IllegalImport
 
 /**
  * Simulates usage of the {@link ValidatedPactProviderRule} on the consumer side.
@@ -29,7 +36,7 @@ public class PactConsumerValidationTest {
     public ValidatedPactProviderRule provider = new ValidatedPactProviderRule("oai/api-test.json", null, "Test", this);
 
     @Rule
-    public CheckForExceptions rule = new CheckForExceptions(provider);
+    public CheckForValidationFailures rule = new CheckForValidationFailures(provider);
 
     @Pact(provider = "Test", consumer = "Test")
     public PactFragment getObjectResponse(final PactDslWithProvider builder) {
@@ -124,14 +131,14 @@ public class PactConsumerValidationTest {
 
     @Test
     @PactVerification(value = "Test", fragment = "getExtraFieldsInObjectResponse")
-    @ExpectExceptionFromRule(expected = ValidatedPactProviderRule.PactValidationError.class)
+    @ExpectValidationErrors("validation.schema.additionalProperties")
     public void fails_whenAdditionalFieldsInResponse_withSimpleObject() {
         get(provider.getConfig().url() + "/test/object");
     }
 
     @Test
     @PactVerification(value = "Test", fragment = "getExtraFieldsInObjectArrayResponse")
-    @ExpectExceptionFromRule(expected = ValidatedPactProviderRule.PactValidationError.class)
+    @ExpectValidationErrors("validation.schema.additionalProperties")
     public void fails_whenAdditionalFieldsInResponse_withArrayOfObjects() {
         get(provider.getConfig().url() + "/test/objectsInArray");
     }
@@ -141,18 +148,18 @@ public class PactConsumerValidationTest {
      */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.METHOD)
-    public @interface ExpectExceptionFromRule {
-        Class<?> expected() default Throwable.class;
+    public @interface ExpectValidationErrors {
+        String[] value() default "";
     }
 
     /**
      * Simple wrapper that allows us to assert the presence of exceptions being emitted by test rules
      */
-    public static class CheckForExceptions implements TestRule {
+    public static class CheckForValidationFailures implements TestRule {
 
         private final TestRule inner;
 
-        public CheckForExceptions(final TestRule inner) {
+        CheckForValidationFailures(final TestRule inner) {
             this.inner = inner;
         }
 
@@ -161,30 +168,36 @@ public class PactConsumerValidationTest {
             return new Statement() {
                 @Override
                 public void evaluate() throws Throwable {
-                    final ExpectExceptionFromRule annotation = description.getAnnotation(ExpectExceptionFromRule.class);
-                    final Class<?> expected = annotation == null ? null : annotation.expected();
+                    final ExpectValidationErrors annotation = description.getAnnotation(ExpectValidationErrors.class);
+                    final String[] expected = annotation == null ? null : annotation.value();
                     try {
                         inner.apply(base, description).evaluate();
                     } catch (final Throwable e) {
-                        if (expected == null) {
-                            throw e;
-                        }
-                        if (!e.getClass().isAssignableFrom(expected)) {
-                            throw new AssertionFailedError(
-                                    format("Expected an exception of type %s but got %s instead",
-                                            expected.getName(), e.getClass().getName()
-                                    )
-                            );
-                        }
+                        checkException(expected, e);
                         return;
                     }
                     if (expected != null) {
-                        throw new AssertionFailedError(
-                                format("Expected an exception of type %s but did not get one", expected.getName())
-                        );
+                        throw new AssertionFailedError("Expected validation errors but did not get any");
                     }
                 }
             };
+        }
+
+        private void checkException(final String[] expected, final Throwable e) throws Throwable {
+            if (expected == null) {
+                throw e;
+            }
+            if (!(e instanceof PactValidationError)) {
+                throw new AssertionFailedError(
+                        format("Expected a validation error but got got %s instead", e.getClass().getName())
+                );
+            }
+            final PactValidationError error = (PactValidationError) e;
+            final Set<String> keys = error.getReport().getMessages().stream().map(Message::getKey).collect(toSet());
+            final Set<String> missingKeys = stream(expected).filter(key -> !key.trim().isEmpty() && !keys.contains(key)).collect(toSet());
+            if (!missingKeys.isEmpty()) {
+                throw new AssertionFailedError(format("Report is missing expected keys: %s. Found %s.", missingKeys, keys));
+            }
         }
     }
 
