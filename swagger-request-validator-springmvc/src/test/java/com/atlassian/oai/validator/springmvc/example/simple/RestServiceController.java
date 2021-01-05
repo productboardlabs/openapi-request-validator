c06b57866d7c1098aa7f71406ee9fceb064d43da
Sven Döring <sven.doering.new@gmail.com>
2021-01-05 22:11:19 +0100

    #312: refactor ResettableRequestServletWrapper for better resettability after partial read
    And adding a benchmark suite to ensure stable performance after refactoring.

--- swagger-request-validator-springmvc/src/test/java/com/atlassian/oai/validator/springmvc/example/simple/RestServiceController.java
+++ swagger-request-validator-springmvc/src/test/java/com/atlassian/oai/validator/springmvc/example/simple/RestServiceController.java
@@ -3,11 +3,14 @@ package com.atlassian.oai.validator.springmvc.example.simple;
 import com.google.common.collect.ImmutableMap;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -25,7 +28,7 @@ public class RestServiceController {
                 .getRequest().getHeader("invalidResponse"));
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{pathVariable}", produces = "application/json")
+    @GetMapping("/{pathVariable}")
     public Map<String, Object> get(@RequestHeader("headerValue") final String headerValue,
                                    @PathVariable("pathVariable") final String pathVariable,
                                    @RequestParam("requestParam") final String requestParam) {
@@ -35,7 +38,7 @@ public class RestServiceController {
         return ImmutableMap.of("headerValue", headerValue, "pathVariable", pathVariable, "requestParam", requestParam);
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    @PostMapping(consumes = "application/json")
     public Map<String, Object> post(@RequestBody final Map<String, Object> body) {
         if (sendInvalidResponse()) {
             return Collections.emptyMap();
@@ -43,7 +46,7 @@ public class RestServiceController {
         return body;
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/{pathVariable}", produces = "application/json")
+    @PutMapping("/{pathVariable}")
     public Map<String, Object> put(@RequestBody final Map<String, Object> body,
                                    @PathVariable("pathVariable") final String pathVariable) {
         if (sendInvalidResponse()) {
@@ -52,11 +55,16 @@ public class RestServiceController {
         return new ImmutableMap.Builder<String, Object>().putAll(body).put("pathVariable", pathVariable).build();
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/{pathVariable}", produces = "application/json")
+    @DeleteMapping("/{pathVariable}")
     public ResponseEntity<Void> delete(@PathVariable("pathVariable") final String pathVariable) {
         if (sendInvalidResponse()) {
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping(value = "/post/blob", consumes = "application/octet-stream")
+    public Map<String, Object> post(@RequestBody final byte[] blob) {
+        return ImmutableMap.of("size", blob.length);
     }
 }
